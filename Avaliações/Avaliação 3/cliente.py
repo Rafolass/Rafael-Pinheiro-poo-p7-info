7188b557d46e1d6e070065438583d6c6f8d11b6c
Rafael <80716003+Rafolass@users.noreply.github.com>
2021-05-17 19:11:00 -0300

    Update cliente.py

--- Avaliações/Avaliação 3/cliente.py
+++ Avaliações/Avaliação 3/cliente.py
@@ -1,6 +1,6 @@
 """
     Módulo cliente -
-    Classe Cliente - 
+    Classe Cliente -
     Atributos:
         _id       - chave primária    - informado
         _nome     - nome do cliente   - informado
@@ -8,10 +8,11 @@
         _cnpjcpf  - cnpj ou cpf       - informado
         _tipo     - tipo do cliente   - informado
                     (Pessoa Fisica ou Juridica)
-        
+
 """
 
-from tipocliente  import TipoCliente
+from tipocliente import TipoCliente
+
 
 class Cliente():
     def __init__(self, id, nome, codigo, cnpjcpf, tipo):
@@ -20,19 +21,22 @@ class Cliente():
         self._codigo = codigo
         self._cnpjcpf = cnpjcpf
         self._tipo = tipo
-        
+
     def str(self):
-        string="\nId={4} Codigo={2} Nome={3} CNPJ/CPF={1} Tipo={0}".format(self._tipo, self._cnpjcpf, self._codigo, self._nome, self._id)
+        string = "\nId={4} Codigo={2} Nome={3} CNPJ/CPF={1} Tipo={0}".format(self._tipo, self._cnpjcpf, self._codigo,
+                                                                             self._nome, self._id)
         return string
-    def getCodigo(self):
+
+    def getCodigoCliente(self):
         return self._codigo
-    def getNome(self):
+
+    def getNomeCliente(self):
         return self._nome
+
     def getCnpjCpf(self):
         return self._cnpjcpf
-    
+
+
 if __name__ == '__main__':
-    cliente=Cliente(1, "Jose Maria", 100, '200.100.345-34', TipoCliente.PESSOA_FISICA)
+    cliente = Cliente(1, "Jose Maria", 100, '200.100.345-34', TipoCliente.PESSOA_FISICA)
     print(cliente.str())
-        
-    
